8d5d6513dbb40a33a6d141ed1fe45c8bcfb4e779
Ravi Gunasekaran <rgunasekaran@altimetrik.com>
2017-12-15 15:06:51 +0530

    Initial commit for TestNG Programs.

---
+++ src/test/java/com/test/testng/programs/ExecutionOrder.java
@@ -0,0 +1,11 @@
+package com.test.testng.programs;
+
+import org.testng.annotations.Test;
+
+public class ExecutionOrder {
+	
+  @Test
+  public void executionOrder() {
+	  System.out.println("   ===   TestNG Execution Order   === ");
+  }
+}
